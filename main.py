9dd469e1affefa0ed1db774d91524ac91137a9bc
Probably-Idiot <zadokfernando@gmail.com>
2024-04-10 20:59:49 +0100

    Removed dark mode switcher, Added blogs pane, Gave real site title

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from flask import *
 app = Flask(__name__)
 
 #app.add_url_rule("/images/cloudsBg.webp", endpoint="/")
+recentBlogs = [["img", "Test Title", "The quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog."],["","",""],["","",""],["","",""],["","",""]]
 
 @app.route("/")
 def home():
-	return render_template("home.htm")
+	return render_template("home.htm", recentBlogs = recentBlogs)
--- templates/home.js
+++
@@ -1,5 +0,0 @@
-$("#light").click(function() {
-	$("#window").css({
-		"background-color": "rgb(32, 28, 58)"
-	});
-});

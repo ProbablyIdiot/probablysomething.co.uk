cc1076eefaa28fc46308e92f96a7367ce336f690
ProbablyIdiot <zadokfernando@gmail.com>
2024-04-17 20:18:16 +0100

    Removed blog title padding in sidebar, added more filler blogs

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import *
 app = Flask(__name__)
 
 #app.add_url_rule("/images/cloudsBg.webp", endpoint="/")
-recentBlogs = [["img", "Test Title", "The quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog."],["","",""],["","",""],["","",""],["","",""]]
+recentBlogs = [["img", "Test Title", "The quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog."],["img", "Test Title", "The quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog."],["img", "Test Title", "The quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog."],["img", "Test Title", "The quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog."],["img", "Test Title", "The quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog, the quick brown fox jumps over the lazy dog."]]
 
 @app.route("/")
 def home():
